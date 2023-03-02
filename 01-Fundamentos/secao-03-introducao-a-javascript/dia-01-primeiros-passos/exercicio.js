d91e137e6bf39923523fdeb71299199e499a8d0f
Nayla Cetinic <nayla.cetinic.z@gmail.com>
2023-03-02 10:19:26 -0300

    +, -, *, / e %

---
+++ 01-Fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/exercicio.js
@@ -0,0 +1,11 @@
+let a = 3;
+let b = 8;
+let c = 20;
+
+//exibir o resultado no console: 
+console.log("Adição:" + (a+b));
+console.log("Subtração:" + (a-b));
+console.log("Multiplicação:" + (a*b));
+console.log("Divisão:" + (a/b));
+console.log("Módulo:" + (a%b));
+
