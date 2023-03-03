d7e0a0b973a7bccb7ee7a81d38210087a31c9366
Nayla Cetinic <nayla.cetinic.z@gmail.com>
2023-03-03 14:47:49 -0300

    transformar porcentagem em nota

--- 01-Fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/exercicio.js
+++ 01-Fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/exercicio.js
@@ -76,3 +76,21 @@ switch(peca.toLocaleLowerCase()) {
     default:
         console.log('Erro, peça inválida!');
 }
+
+//transformar porcentagem em nota 
+let nota = 100;
+if (nota >= 90 && nota <= 100) {
+    console.log("A");
+} else if (nota >= 80 && nota <= 90) {
+    console.log("B");
+} else if (nota >= 70 && nota <= 80) {
+    console.log("C");
+} else if (nota >= 60 && nota <= 70) {
+    console.log("D");
+} else if (nota >= 50 && nota <= 60) {
+    console.log("E");
+} else if (nota < 50>) {
+    console.log("F");
+} else {
+    console.log("Erro, nota incorreta!");
+}
